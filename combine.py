4d2f4e9203d4e7199997e0c1c75673151cfb4d3d
Theodore Morley <theodore.morley@mail.mcgill.ca>
2016-02-21 21:34:51 -0500

    Combination script for use in matlab scripts. Takes three inputs, (1) experiment file (2) results file (3) name of output file

---
+++ combine.py
@@ -0,0 +1,34 @@
+#PLAN: take two input files, and an output name(3 args). The first input is the original experiment file, the second is
+#the experiment results.
+#First, match until end of line on the first file, and place it in the new file. Then put a tab, and grab the trial data
+import sys
+import re
+import json
+
+originalExperiment=sys.argv[1]
+trialDataFile=sys.argv[2]
+outputFileName=sys.argv[3]
+
+f=open(originalExperiment)
+
+experimentData=re.split(r"(?:\r\n)|(?:\n)|(?:\r)",f.read())
+
+f.close()
+
+f2=open(trialDataFile)
+trialDataList=re.split(r"(?:\r\n)|(?:\n)|(?:\r)",f2.read())
+
+f2.close()
+nonHeadersString=''.join(experimentData[1:len(experimentData)])
+
+trialDataListWithNonHeaders=[]
+for i in xrange(len(trialDataList)):
+    if len(trialDataList[i])>0 and not trialDataList[i][0]=="#":
+        trialDataList[i]=trialDataList[i].replace(",", "\t")+nonHeadersString
+
+out=open(outputFileName, 'w')
+out.write(experimentData[0]+"\n")
+for line2 in trialDataList:
+    if len(line2)>0 and not line2[0]=="#":
+        out.write(line2+"\n")
+out.close()
