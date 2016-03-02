d815f32055b521231103bd97b30fdb95711f9a3b
Theodore Morley <theodore.morley@mail.mcgill.ca>
2016-03-02 01:45:02 -0500

    Changed to a different method of picking data from experiment file, trying to grab data common over the whole file to append to trials, not sure if extensible.

--- combine.py
+++ combine.py
@@ -23,13 +23,52 @@ trialDataList=re.split(r"(?:\r\n)|(?:\n)|(?:\r)",f2.read())
 f2.close()
 nonHeadersString=''.join(experimentData[1:len(experimentData)])
 
+#grab the experiment, status, animacy, qtype, design, and record for the first column(Dumb version, will assume these are
+#the same, since I'm unsure which parts of the data are necesary to include on each participant line 
+#experiment
+experimentType=re.match(r"^(.*?)\t",experimentData[1])
+#status
+status=re.match(r"^((.*?\t.*?){5})",experimentData[1])
+#animacy
+animacy=re.match(r"^((.*?\t.*?){6})",experimentData[1])
+#qtype
+qtype=re.match(r"^((.*?\t.*?){11})",experimentData[1])
+#design
+design=re.match(r"^((.*?\t.*?){12})",experimentData[1])
+#recordVar
+recordVar=re.match(r"^((.*?\t.*?){13})",experimentData[1]+"\t")
+
+#testing the check for which one this found
+#for posterity, the number in brackets is the column number of the part I want, the first half of the tuple
+#contains everything before it
+#print status.groups()[1]
+
+copiedDataString=experimentType.groups()[0]+"\t"+status.groups()[1]+animacy.groups()[1]+qtype.groups()[1]+design.groups()[1]+recordVar.groups()[1]
+
+#Next, we need to get the correct list of column headers for the selected experimentData columns
+#thankfully we basically already have the code for this
+#experiment
+experimentTypeh=re.match(r"^(.*?)\t",experimentData[0])
+#status
+statush=re.match(r"^((.*?\t.*?){5})",experimentData[0])
+#animacy
+animacyh=re.match(r"^((.*?\t.*?){6})",experimentData[0])
+#qtype
+qtypeh=re.match(r"^((.*?\t.*?){11})",experimentData[0])
+#design
+designh=re.match(r"^((.*?\t.*?){12})",experimentData[0])
+#recordVar
+recordVarh=re.match(r"^((.*?\t.*?){13})",experimentData[0]+"\t")
+
+copiedHeaderString=experimentTypeh.groups()[0]+"\t"+statush.groups()[1]+animacyh.groups()[1]+qtypeh.groups()[1]+designh.groups()[1]+recordVarh.groups()[1]
+
 trialDataListWithNonHeaders=[]
 for i in xrange(len(trialDataList)):
     if len(trialDataList[i])>0 and not trialDataList[i][0]=="#":
-        trialDataList[i]=nonHeadersString + "\t" + trialDataList[i].replace(",", "\t")
+        trialDataList[i]=copiedDataString + trialDataList[i].replace(",", "\t")
 
 out=open(outputFileName, 'w')
-out.write(experimentData[0]+"\n")
+out.write(copiedHeaderString+"\n")
 for line2 in trialDataList:
     if len(line2)>0 and not line2[0]=="#":
         out.write(line2+"\n")
